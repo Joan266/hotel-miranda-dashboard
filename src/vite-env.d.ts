bec2baad1cbebc48417de6e100a218265d3f6af2
Joan <joanalemany26@gmail.com>
2024-08-01 10:35:39 +0200

    last commit of this branch

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_PUBLIC_API_DOMAIN: string
-  // more env variables...
+  readonly VITE_PUBLIC_API_DOMAIN: string;
 }
 
 interface ImportMeta {
-  readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
