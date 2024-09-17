028a4b58e466a5feefd4427028a94a54f3578210
Joan <joanalemany26@gmail.com>
2024-09-17 23:06:42 +0200

    removed form modal added custom user form

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BookingDetails } from './pages/bookings/BookingDetails';
 import { Reviews } from './pages/reviews/Reviews';
 import { Rooms } from './pages/rooms/Rooms';
 import { Users } from './pages/users/Users';
+import { UserDetails } from './pages/users/UserDetails';
 import { Login } from './pages/Login';
 import { Layout } from './pages/Layout';
 import { PrivateRoute } from './components/PrivateRoute';
@@ -45,6 +46,7 @@ export const App = () => {
             <Route path="reviews" element={<Reviews />} />
             <Route path="rooms" element={<Rooms />} />
             <Route path="users" element={<Users />} />
+            <Route path="users/:id" element={<UserDetails />} />
           </Route>
           <Route path="/login" element={<Login/>} />
         </Routes>
--- src/pages/bookings/BookingDetails.jsx
+++ src/pages/bookings/BookingDetails.jsx
@@ -1,7 +1,5 @@
 import { useParams } from 'react-router-dom';
 export const BookingDetails = () => {
   const { id } = useParams();
-  return(
-    <h1>Booking Details id: {id}</h1>
-  )
+
 }
